ed265470b71ca7810cdc4e7728d18549f2504d37
Ritika Mondal <ritikamondal729@gmail.com>
2026-08-05 05:29:35 +0000

    container config done

---
+++ .devcontainer/js_basics/js_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1666665
+let accountEmail = "ritika@jk.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+
+//accountId = 2
+
+accountEmail = "ritesh@jk.com"
+accountPassword = "9999999999"
+accountCity = "diamond harbour"
+
+console. log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity])
